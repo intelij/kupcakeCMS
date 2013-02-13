a68ed65aa142450e0832130ae3d227686e9fe941
intelij <k.mkhonza@yahoo.co.uk>
2013-02-13 11:50:43 +0000

    Update genThumbsnail.php

--- genThumbsnail.php
+++ genThumbsnail.php
@@ -1,6 +1,4 @@
-###Image Generator using
 
-```php
 <?php
 Class genThumbsnail {      
   
@@ -17,21 +15,15 @@ Class genThumbsnail {
 	$original_file = str_replace("../..", $_SERVER['DOCUMENT_ROOT'], $original_file); 
 	  
 	   // add shell command
-	  $image_magick = "convert '$original_file'  -strip -quality 80% -resize 90x90^  -gravity center -extent 90x90  '$destination_file'";  
+	$image_magick = "convert '$original_file'  -strip -quality 80% -resize 90x90^  -gravity center -extent 90x90  '$destination_file'";  
 
-    $output = exec($image_magick);
+    	$output = exec($image_magick);
 
-	
-         // exec("convert assets/20th_century_fox/get_the_gringo/images/SV-04425.JPG  -strip -quality 80% -resize 90x90^  -gravity center -extent 90x90 assets/20th_century_fox/get_the_gringo/images/THUMBS2_SV-04425.JPG", $blaArray, $responsev);
-	
-	// echo $output; 
-	
-	// echo "<script>javascript:alert('Generating Thumbs!');</script>";
+
 
 	}   
 	
-	//create_square_image("sample.jpg","thumbs_sample.jpg",90);   
+
 	
 }
 
-```
